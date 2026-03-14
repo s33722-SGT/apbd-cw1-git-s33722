f573361b51936ae5e48420ace35c5571eb8125f9
Piotr Sztenke <s33722@pjwstk.edu.pl>
2026-03-14 11:16:17 +0100

    dodalem sprawdzanie czy values nie jest puste, tym razem zamiast return 0 jest return None w CalculateMin

--- metody.py
+++ metody.py
@@ -10,6 +10,8 @@ def CalculateAverage(values):
 print(CalculateAverage([1,9,3,4,5]))
 
 def CalculateMax(values):
+    if not values:
+        return None
     return max(values)
 
 def CalculateMin(values):
